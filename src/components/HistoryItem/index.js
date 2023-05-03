88ae440c79132cf00965e9808e873f92e24d0e1f
shabana888 <shaikshabana.btc@gmail.com>
2023-05-03 08:06:47 +0000

    browserHistory

---
+++ src/components/HistoryItem/index.js
@@ -0,0 +1,39 @@
+import './index.css'
+
+const HistoryItem = props => {
+  const {HistoryDetails, deleteHistory} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = HistoryDetails
+
+  const onDelete = () => {
+    deleteHistory(id)
+  }
+  return (
+    <li className="history-item">
+      <div className="browser-start">
+        <p className="history-time">{timeAccessed}</p>
+        <div className="browser-middle">
+          <img src={logoUrl} className="history-image" alt="domain logo" />
+          <p className="history-title">{title}</p>
+          <p className="history-url">{domainUrl}</p>
+        </div>
+      </div>
+      <div className="browser-end">
+        <button
+          className="delete-button"
+          type="button"
+          onClick={onDelete}
+          // eslint-disable-next-line react/no-unknown-property
+          data-testid="delete"
+        >
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            className="delete-image"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
